20943e172029ca2a26807c318dab2062047db2d0
komiya <owl20050127@gmail.com>
2024-02-04 21:02:46 +0900

    test

--- sd/decoder.py
+++ sd/decoder.py
@@ -46,7 +46,12 @@ class VAE_ResidualBlock(nn.Module):
 
         self.groupnorm_2=nn.GroupNorm(32,out_channels)
         self.conv_2=nn.Conv2d(in_channles,out_channels,kernel_size=3,padding=1)
-
+        
+        #スキップ接続
+        #層を飛ばして接続する
+        #入力チャンネル数と出力チャンネル数が異なる時、
+        #なぜか
+        #xのチャンネル数は入力チャンネル数、forwardメソッドのreturnでx+residueとしたときに次元数が合わないかららしい
         if in_channles==out_channels:
             self.residual_layer=nn.Identity()
         else:
@@ -57,10 +62,10 @@ class VAE_ResidualBlock(nn.Module):
 
 
 #ここは何をしているのか
-        residue=x
-        x=self.groupnorm_1(x)
+        residue=x  #入力xを残差として保存
+        x=self.groupnorm_1(x)  #グループ正規化
 
-        x=F.silu(x)
+        x=F.silu(x) #活性化関数　なぜか学習プロセスが安定するらしい
 
         x=self.conv_1(x)  #paddingしてから畳み込みなので画像のサイズは同じ
 
@@ -68,9 +73,75 @@ class VAE_ResidualBlock(nn.Module):
 
         x=F.silu(x)
 
-        x=self.conv_2(x)
+        x=self.conv_2(x) #形状は同じ
 
-        return x+self.residual_layer(residue)
+        return x+self.residual_layer(residue) 
 
         #スキップ接続
+
+#デコーダー内に残差ブロックがいくつもあるのはなぜか
+    
+class VAE_Decoder(nn.Sequential):
+
+    def __init__(self):
+        super().__init__(
+
+            #デコーダーでは潜在表現の次元数から元の画像のサイズに拡大する
+            nn.Conv2d(4,4,kernel_size=1,padding=0),
+
+            nn.Conv2d(4,512,kernel_size=3,padding=1),
+
+            VAE_ResidualBlock(512,512),
+
+            VAE_AttentionBlock(512,),
+
+            VAE_ResidualBlock(512,512),
+            VAE_ResidualBlock(512,512),
+            VAE_ResidualBlock(512,512),
+
+            #畳み込みがないので(Batch,512,Height/8,Width/8)
+            VAE_ResidualBlock(512,512),
+            
+            #(Batch_Size,512,Height/8,Width/8) -> (Batch_Size,512,Height/4,Width/4)
+            nn.Upsample(scale_factor=2),   #アップサンプルでは何をしているのか
+
+            nn.Conv2d(512,512,kernel_size=3,padding=1),
+
+            VAE_ResidualBlock(512,512),
+            VAE_ResidualBlock(512,512),
+            VAE_ResidualBlock(512,512),
+            
+            #(Batch_Size,512,Height/4,Width/4) -> (Batch_Size,512,Height/2,Width/2)
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(512,512,kernel_size=3,padding=1),
+            
+            #特徴の数を減らす
+            VAE_ResidualBlock(512,256),
+            VAE_ResidualBlock(256,256),
+            VAE_ResidualBlock(256,256),
+            
+            #(Batch_Size,256,Height/2,Width/2) -> (Batch_Size,256,Height,Width)
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(256,256,kernel_size=3,padding=1),
+
+            VAE_ResidualBlock(256,128),
+            VAE_ResidualBlock(128,128),
+            VAE_ResidualBlock(128,128),
+
+            nn.GroupNorm(32,128), #32グループ、特徴量は128次元
+
+            nn.SiLU(),
+            
+            #(Batch_Size,128,Height,Width) -> (BAtch_Size,3,Height,Width)
+            #入力の128チャンネルからRGBの3チャンネルにする    conv2d(入力チャンネル、出力チャンネル、カーネルサイズ、パディング)
+            nn.Conv2d(128,3,kernel_size=3,padding=1)
+        )
+        
+    def forward(self,x:torch.Tensor)  ->torch.Tensor:
+
+        x/=0.18215
+        for module in self:
+            x=module(x)
+        return x
+
 
--- sd/encoder.py
+++ sd/encoder.py
@@ -13,7 +13,10 @@ class VAE_Encoder(nn.Sequential):
             
             #次のブロック  第一引数は入力チャンネル数,第二引数は出力チャンネル数
             
-            #(Batch_Size,128,Height,Width)ー> (Batch_Size,128,Height,Width) 
+            #(Batch_Size,128,Height,Width)ー> (Batch_Size,128,Height,Width)
+            #残差ブロック
+            #CNNは層を深くすると勾配消失があったが残差ブロックを使うと勾配消失が起きにくくなる
+            #なぜ　調べてもよく分からないので質問
             VAE_ResidualBlock(128,128),  #畳み込みとnormalizationが合わさっている
             
             #(Batch_Size,128,Height,Width)ー> (Batch_Size,128,Height 　/  2 , Width  /  2) 
@@ -21,7 +24,7 @@ class VAE_Encoder(nn.Sequential):
             nn.Conv2d(128,128,kernel_size=3,stride=2,padding=0),#1行前でoutputが128チャンネルなのでこの行は入力128チャンネル   ネットのonvolutional visualizerというのを使って説明していた
             
             #(Batch_Size,128,Height/2 ,Width/2  )ー> (Batch_Size,256,Height 　/  2 , Width  /  2) 
-            VAE_ResidualBlock(128,256), #featureの数を増やした
+            VAE_ResidualBlock(128,256), #featureの数を増やした　入力の特徴量が128で出力の特徴量が256ということ
             
             #(Batch_Size,256,Height/2 ,Width/2  )ー> (Batch_Size,256,Height 　/  2 , Width  /  2) 
             VAE_ResidualBlock(256,256),
@@ -53,22 +56,28 @@ class VAE_Encoder(nn.Sequential):
             #それぞれのピクセルに対するself attentionとして動く
             #Attentionは文中でトークンを関連づける(?)1:00:40 　ピクセルの列として捉えられAttentionはそれぞれのピクセルを関連づける方法として捉えられる
             #畳み込みでも近接するピクセルは関連づけられたが、Attentionは近接していないピクセルも関連づける
+            #512次元の特徴量マップが入力、アタンションで入力特徴量の中から特定の情報に焦点を当てる
+            #焦点を当てた情報から有意義な特徴量表現を生成しVAEモデルの後続部分に渡す
             VAE_AttentionBlock(512),
             
             #(Batch_Size,512,Height/8 ,Width/8  )ー> (Batch_Size,512,Height 　/  8 , Width  /  8) 
             VAE_ResidualBlock(512,512),
 
             #(Batch_Size,512,Height/8 ,Width/8  )ー> (Batch_Size,512,Height 　/  8 , Width  /  8) 
+            #バッチ正規化とは異なる　第一引数はグループの数、チャンネルが幾つのグループに分割されるか　第二引数は正規化を適用するチャンネルの総数
             nn.GroupNorm(32,512),
 
             #(Batch_Size,512,Height/8 ,Width/8  )ー> (Batch_Size,512,Height 　/  8 , Width  /  8) 
-            nn.SiLU(),    #これを用いるのはこのようなモデルで一般的によく使われなぜこのようなモデルに適しているのかは不明らしい,この人のLAMAの動画でも紹介したらしい
+            nn.SiLU(),    #この活性化関数を用いるのはこのようなモデルで一般的によく使われなぜこのようなモデルに適しているのかは不明らしい,この人のLAMAの動画でも紹介したらしい
 
             #(Batch_Size,512,Height/8 ,Width/8  )ー> (Batch_Size,8,Height 　/  8 , Width  /  8) 
             #strideは1でパディングをしているので画像の大きさは変わらない
+            #conv2d(入力チャンネル数、出力チャンネル数、カーネルサイズ、パディング)
+            #padding=1なので入力テンソルの各辺に1ピクセルずつ0パディング
             nn.Conv2d(512,8,kernel_size=3,padding=1)
             
             #(Batch_Size,8,Height/8 ,Width/8  )ー> (Batch_Size,8,Height /  8 , Width  /  8) 
+            #padding=0なのでパディングは追加されない
             nn.Conv2d(8,8,kernel_size=1,padding=0)
 
             #なぜカーネルサイズ1で畳み込みしてるのか、畳み込みになっていないのではないか
@@ -83,27 +92,30 @@ def forward(self,x:torch.Tensor,noise:torch.Tensor)->torch.Tensor:
     #getattr関数は、第一引数に指定されたオブジェクトから、第二引数で指定された名前の属性を取得しようとします。もし属性が存在しない場合は、第三引数で指定されたデフォルト値（この場合はNone）を返します。
     for module in self:
         if getattr(module,'stride',None)==(2,2): #各モジュールからstride属性を取得しその値が(2,2)であるかどうかを確認
+            
+            #各モジュールからstride属性を取得というのはどういうことか
+
             #(Padding Left, Padding Right, Padding Top, Padding Bottom)
             x= F.pad(x,(0,1,0,1))  #なぜ(0,1,0,1)
         x=module(x)
-    
-    #ここから数行は何をしているのか
 
     #(Batch_Size,8,Height/8 ,Width/8  )  -> two tensors of shape (Baatch_size,4, Height, Width)
-    mean,log_varianece=torch.chun(x,2,dim=1)
+    #指定されたdimに沿って入力テンソルxを2このテンソルに均等に分割する関数
+    #分割した1つめのテンソルはmeanに、2つめのテンソルはlog_varianceとする
+    mean,log_varianece=torch.chunk(x,2,dim=1) 
 
     #(Batch_Size,4,Height/8,Width/8) -> (Batch_Size,4,Height/8, Width / 8)
-    variance=log_varianece.exp()
+    variance=log_varianece.exp()  #logvarianceの各要素に対して指数を適用 e^(log_variance)
     
     #サイズ変化はなし
-    stdev=variance.sqrt()
+    stdev=variance.sqrt() #平方根
 
     #Z= N(0,1)から取り出す N(mean,variance)
     #x= mean +stdev *Z
-    x=mean +stdev*noise
+    x=mean +stdev*noise  #noiseはforwardの引数でテンソル型、正規分布からサンプリングされている
 
     #定数によって出力をscale
-    x*=0.18215
+    x*=0.18215  #なぜこの値なのか
 
     return x
     
